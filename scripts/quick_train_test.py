7450cd15d40cacdc514b646d57c10ecd2c153881
malerbe <63046315+malerbe@users.noreply.github.com>
2025-08-16 14:59:06 +0200

    Added main folders, started writing scripts

---
+++ scripts/quick_train_test.py
@@ -0,0 +1,20 @@
+# author: Louca Malerba
+# date: 16/08/2025
+
+# Public libraries importations
+import torch
+import torch.nn as nn
+import torch.nn.functional as functional
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision
+from torchvision import datasets, transforms
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import os
+from tqdm.auto import tqdm
+import sys
+
+
+
